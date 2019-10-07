a001f68c0b9c898288f40cf32b08d31e69ee7b60
Marcuss17 <eduardo.medina@utec.edu.pe>
2019-10-06 23:17:23 -0500

    Get all users now works ok

--- web/static/js/chat.js
+++ web/static/js/chat.js
@@ -6,14 +6,13 @@ function get_all_users(){
     console.log("Voy a traer todos los usuarios");
 
     $.getJSON("/users", function(data){
-    var i = 0;
-    $.each(data,function(){
+    var i =0;
+    $.each(data, function(){
         user_to = data[i]['id'];
-        e = '<div class="alert" role="alert" >';
-        e = e+'<div>'+data[i]['username']+'</div>';
-        e = e+'</div>';
+        e = '<li class="contact"><p>'data[i]['username']+'</p></li>';
         i = i+1;
         $("<div/>",{html:e}).appendTo("#users");
         });
     });
 }
+
