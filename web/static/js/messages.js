9cf263bf6dc61d9e20704f493fbc1869fa6b6551
Marcuss17 <eduardo.medina@utec.edu.pe>
2019-10-22 00:48:11 -0500

    up to date

--- web/static/js/messages.js
+++ web/static/js/messages.js
@@ -1,58 +1,80 @@
-function getMessagesDevEx(){
+$(function(){
     var url = "http://127.0.0.1:8000/messages";
-     $("#grid").dxDataGrid({
-         dataSource: DevExpress.data.AspNet.createStore({
-             key: "id",
-             insertUrl: url,
-             updateUrl: url,
-             deleteUrl: url,
-             loadUrl: url,
-             onBeforeSend: function(method, ajaxOptions) {
-                 ajaxOptions.xhrFields = { withCredentials: true };
-             }
-         }),
+    var url_users = "http://127.0.0.1:8000/users";
+    $("#grid").dxDataGrid({
+        dataSource: DevExpress.data.AspNet.createStore({
+            key: "id",
+            loadUrl: url ,
+            insertUrl: url ,
+            updateUrl: url ,
+            deleteUrl: url ,
+            onBeforeSend: function(method, ajaxOptions) {
+                ajaxOptions.xhrFields = { withCredentials: true };
+            }
+        }),
 
-         editing: {
-             allowUpdating: true,
-             allowDeleting: true,
-             allowAdding: true
-         },
+        editing: {
+            allowUpdating: true,
+            allowDeleting: true,
+            allowAdding: true
+        },
 
-         remoteOperations: {
-             sorting: true,
-             paging: true
-         },
+        remoteOperations: {
+            sorting: true,
+            paging: true
+        },
 
-         paging: {
-             pageSize: 12
-         },
+        paging: {
+            pageSize: 12
+        },
 
-         pager: {
-             showPageSizeSelector: true,
-             allowedPageSizes: [8, 12, 20]
-         },
+        pager: {
+            showPageSizeSelector: true,
+            allowedPageSizes: [8, 12, 20]
+        },
 
-         columns: [{
-             dataField: "id",
-             dataType: "number",
-             allowEditing: false
-         }, {
-             dataField: "content"
-         }, {
-             dataField: "sent_on",
-             allowEditing: false
-         }, {
-             dataField: "user_from_id"
-         }, {
-             dataField: "user_to_id"
-         },{
-             dataField: "user_from"
-         },{
-             dataField: "user_to"
-         }]
-
-     }).dxDataGrid("instance");
-
-
-
-}
+        columns: [{
+            dataField: "id",
+            dataType: "number",
+            allowEditing: false
+        }, {
+            dataField: "content"
+        }, {
+            dataField: "sent_on",
+            dataType: "date",
+            format: "MM/dd/yyyy hh:mm"
+        }, {
+            dataField: "user_from_id",
+            lookup: {
+                    dataSource:  DevExpress.data.AspNet.createStore({
+                        key: "id",
+                        loadUrl: url_users ,
+                        insertUrl: url_users ,
+                        updateUrl: url_users ,
+                        deleteUrl: url_users ,
+                        onBeforeSend: function(method, ajaxOptions) {
+                            ajaxOptions.xhrFields = { withCredentials: true };
+                        }
+                    }),
+                    valueExpr: "id",
+                    displayExpr: "username"
+                }
+        }, {
+            dataField: "user_to_id",
+            lookup: {
+                    dataSource:  DevExpress.data.AspNet.createStore({
+                        key: "id",
+                        loadUrl: url_users ,
+                        insertUrl: url_users ,
+                        updateUrl: url_users ,
+                        deleteUrl: url_users ,
+                        onBeforeSend: function(method, ajaxOptions) {
+                            ajaxOptions.xhrFields = { withCredentials: true };
+                        }
+                    }),
+                    valueExpr: "id",
+                    displayExpr: "username"
+                }
+        } ],
+    }).dxDataGrid("instance");
+});
